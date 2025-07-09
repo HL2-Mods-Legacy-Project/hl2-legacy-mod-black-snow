a3fed5e3154aeaaf752153bd153088308f25e598
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 19:30:05 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Black_Snow_1.05" `
+  -ModName "Black Snow 1.05" `
+  -ModFolder "blacksnow" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/black-snow/blacksnow-1.05-Original.zip" `
+  -ModBaseFilesUrlHash "bba078680a2a2f30f0c4337746faa3ef2ee3d8df1c187b3355996c0e8103bac0" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
